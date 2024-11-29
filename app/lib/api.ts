4a2e211b787cab348afb7c66ebc0a526302bb8aa
brunobertapeli <zerocodeceo@gmail.com>
2024-11-29 09:59:03 -0500

    3334

---
+++ app/lib/api.ts
@@ -0,0 +1,17 @@
+const getApiUrl = () => {
+  // Check if we're in the browser
+  if (typeof window === 'undefined') {
+    // Return the production URL during server-side rendering
+    return process.env.NEXT_PUBLIC_PROD_API_URL
+  }
+
+  // Client-side logic
+  const isProduction = window.location.hostname.includes('vercel.app')
+  const apiUrl = isProduction 
+    ? process.env.NEXT_PUBLIC_PROD_API_URL 
+    : process.env.NEXT_PUBLIC_API_URL
+
+  return apiUrl
+}
+
+export const API_URL = getApiUrl()
