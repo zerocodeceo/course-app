022849723ea9f6eaf6badf1de7101118da1da78c
brunobertapeli <95784880+brunobertapeli@users.noreply.github.com>
2024-11-29 09:16:16 -0500

    bug
    g

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/no-explicit-any': ['error'],
-    'react-hooks/exhaustive-deps': ['warn']
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 } 
