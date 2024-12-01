e391a047ed948f79797c18c6e79bf005a916c398
brunobertapeli <zerocodeceo@gmail.com>
2024-12-01 15:16:26 -0500

    changinf video

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
             <div className="relative pt-[56.25%]">
               <iframe
                 className="absolute top-0 left-0 w-full h-full rounded-xl shadow-lg"
-                src="https://www.youtube.com/embed/oBkJUIFc-ak"
+                src="https://www.youtube.com/embed/m4HZgYcyUVA"
                 title="YouTube video"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
