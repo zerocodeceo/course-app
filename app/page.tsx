6eef4a4a238e7054a9ead6a0621b5d787bed9a20
brunobertapeli <zerocodeceo@gmail.com>
2024-12-04 16:20:42 -0500

    443

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             <div className="relative pt-[56.25%]">
               <iframe
                 className="absolute top-0 left-0 w-full h-full rounded-xl shadow-lg"
-                src="https://www.youtube.com/embed/Gnke_yRILJA"
+                src="https://www.youtube.com/embed/0BdmVe6GXXI"
                 title="Intro to ZeroCodeCEO"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
